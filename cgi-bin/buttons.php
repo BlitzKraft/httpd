53b38bb8c697fa72dd8aae8ca340a1e05264fef9
BK Bolisetty <bk@squarefruitlabs.com>
2015-12-11 20:59:38 -0600

    Ready to switch branches

--- cgi-bin/buttons.php
+++ cgi-bin/buttons.php
@@ -1,8 +1,7 @@
 <ul class="buttonlist" id="tabs">
 	<li class="button slideLeft2"><a href="" onclick="showTab()"><span></span>Navigate</a> </li>
-	<li class="button slideLeft2"><a href="" onclick="showTab()"><span></span>Upload file</a></li>
 	<li class="button slideLeft2"><a href=""><span></span>Track and follow</a></li>
-	<li class="button slideLeft2"><a href="" onclick="showTab()"><span></span>Arm</a></li>
+	<li class="button slideLeft2"><a href="" onclick="showTab()"><span></span>Upload file</a></li>
 	<li class="button slideLeft2"><a href="" onclick="showTab()"><span></span>Persona</a></li>
 	<li class="button slideLeft2"><a href="" onclick="showTab()"><span></span>Settings</a></li>
 	<li class="button slideLeft2"><a href="" onclick="showTab()"><span></span>Advanced</a></li>
