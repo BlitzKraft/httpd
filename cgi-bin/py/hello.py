44da825f690251ecdf5adc9453b78202a75876c8
BK Bolisetty <bk@squarefruitlabs.com>
2015-12-23 16:46:56 -0600

    Changed the directory structure. Everything is properly placed at webroot

--- cgi-bin/py/hello.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/python
-#
-print "Content-Type: text/html\n\n"
-print '<html><head><meta content="text/html; charset=UTF-8" />'
-print '<title>Persona on Beagle</title><p>'
-for count in range(1,100): 
-  print 'Hola! '
-print "</p></body></html>"
--- gui/php/test.cgi
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "success from var" > /dev/pts/1
